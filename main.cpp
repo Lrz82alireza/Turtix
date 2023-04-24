2d1b09d85819fe1ff8c819d794417f62b01723d1
alireza <lrz82mirshafee@gmail.com>
2023-04-24 21:53:28 +0330

    complete Makefile for main

--- main.cpp
+++ main.cpp
@@ -2,4 +2,23 @@
 
 int main()
 {
+    sf::RenderWindow window(sf::VideoMode(700, 200), "SFML works!");
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
+
+    return 0;
 }
