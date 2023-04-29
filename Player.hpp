a10ef5010abb32cefe87f39325e68ffc32b57aad
alireza <lrz82mirshafee@gmail.com>
2023-04-29 23:56:17 +0330

    player

---
+++ Player.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <SFML/Graphics.hpp>
+#include <SFML/System.hpp>
+#include <SFML/Window.hpp>
+#include <SFML/Audio.hpp>
+#include <SFML/Network.hpp>
+
+class Player
+{
+private:
+    /* data */
+public:
+    Player(/* args */);
+    ~Player();
+};
+
